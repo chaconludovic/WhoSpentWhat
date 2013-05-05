1d34bef401747be9ed24f9d9be11c0dd6dd4b336
chaconludovic <ludovic_chacon@hotmail.com>
2013-05-05 20:33:42 +0200

    second commit

---
+++ src/com/eldoraludo/whosspentwhat/WhoSSpentWhat.java
@@ -0,0 +1,15 @@
+package com.eldoraludo.whosspentwhat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhoSSpentWhat extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
